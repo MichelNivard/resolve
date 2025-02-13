52a1bf1e1753e62e794c75af4aedc298e71af16d
Michel Nivard <m.g.nivard@gmail.com>
2025-02-13 18:36:49 +0100

    chekzzz

--- frontend/src/components/Editor/EditorWrapper.jsx
+++ frontend/src/components/Editor/EditorWrapper.jsx
@@ -105,6 +105,7 @@ const EditorWrapper = ({
       CitationMark,
     ],
     content: '',
+    enableContentCheck: true,
     editorProps: {
       attributes: {
         class: 'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
