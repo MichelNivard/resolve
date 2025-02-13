c3d3bd7c5c5e140e1d11e493b28db2599fcb4162
Michel Nivard <m.g.nivard@gmail.com>
2025-02-13 15:55:20 +0100

    try that

--- frontend/src/utils/notebookConversionUtils.js
+++ frontend/src/utils/notebookConversionUtils.js
@@ -49,7 +49,7 @@ export function ipynbToTiptapDoc(ipynb, editor) {
 
   // Create a transaction to set content with marks preserved
   const tr = editor.state.tr;
-  // tr.setMeta('trackManualChanged', true);
+  tr.setMeta('trackManualChanged', false);
 
   for (const cell of cells) {
     if (cell.type === 'raw') {
